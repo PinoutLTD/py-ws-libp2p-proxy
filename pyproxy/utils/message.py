33b07d74e1ef00b1341ad01d414d8929e424ab27
tubleronchik <m.bystramovich@gmail.com>
2024-04-02 11:55:59 +0200

    change msgs' format

--- pyproxy/utils/message.py
+++ pyproxy/utils/message.py
@@ -1,16 +1,14 @@
-import copy
+import typing as tp
 import json
 
 
-def format_msg_from_libp2p(message: dict) -> dict:
-    protocol = message.get("protocol")
-    message_copy = copy.deepcopy(message)
-    message_copy.pop("protocol", None)
-    return message_copy
+def format_msg_from_libp2p(message: dict) -> tp.Union[str, dict]:
+    data = message["data"]
+    return data
 
 
 def format_msg_to_libp2p(data: str, protocol: str, server_peer_id: str, save_data: bool) -> str:
-    message = {"protocol": protocol, "serverPeerId": server_peer_id, "save_data": save_data, "data": data}
+    message = {"protocol": protocol, "serverPeerId": server_peer_id, "save_data": save_data, "data": {"data": data}}
     return json.dumps(message)
 
 
